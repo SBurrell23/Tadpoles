e48993c74ab97d50d8400d51d27416ad9f4aff66
Steven Burrell <sburrell23@gmail.com>
2023-12-26 22:06:39 -0700

    removed bad text

--- GameClient.js
+++ GameClient.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://tadpoles.onrender.com'); //stayaway.onrender.com
+const socket = new WebSocket('ws://localhost:8080'); //tadpoles.onrender.com
 socket.addEventListener('open', function () {
     socket.addEventListener('message', function (event) {
         handleServerMessage(event.data);
